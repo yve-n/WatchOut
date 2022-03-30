55d32c5a0f3e401c750e5ab1e047ccc15bcebee1
yve-n <nguetcheyvecarelle@gmail.com>
2022-03-30 23:32:23 +0200

    first test to start

--- App.js
+++ App.js
@@ -1,19 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
+function HomeScreen ({navigation}){
+  return(
+    <View style={styles.container}>
+      <Text>Home screen </Text>
+      <Button title='Go to details' onPress={()=>navigation.navigate('Details')}/>
+    </View>
+  )
+}
+function DetailsScreen (){
+  return(
+    <View style={styles.container}>
+      <Text>Details screen </Text>
+    </View>
+  )
+}
+const {Navigator, Screen }= createStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Navigator>
+        <Screen name="Home" component={HomeScreen} options={{title:'Overview'}}/>
+        <Screen name="Details" component={DetailsScreen} />
+      </Navigator>
+    </NavigationContainer>
+    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
   },
